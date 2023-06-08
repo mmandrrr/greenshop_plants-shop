6bdfc5797983c9d4d04cc7ab54abb71fd20d390e
Maxim <mmandrrr@gmail.com>
2023-06-08 18:44:25 +0300

    fixed bug with active style in setActiveItem service

--- src/services/setActiveItem.js
+++ src/services/setActiveItem.js
@@ -1,5 +1,5 @@
 export const setActiveItem = (e) => {
-    const item = document.querySelectorAll(`.${e.target.classList}`);
+    const item = document.querySelectorAll(`.${e.target.classList[0]}`);
 
     item.forEach(item => {
         item.classList.remove('active');
